c66773d81c48ea5b82c7e15233c9e8158ecfe66e
Dominggoes Isakh <drjisakh@gmail.com>
2025-08-18 07:43:28 -0400

    universal8890-common: Drop manual copy of libprotobuf
    Change-Id: Ia4ad6413b2ca3be66cb46adb32397ef57a8a4e83

--- device-common.mk
+++ device-common.mk
@@ -390,12 +390,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
 
-# VNDK prebuilts
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
-
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
