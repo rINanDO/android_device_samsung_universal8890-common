811e452be67b197bf557a6d6700c5c5dbaa64954
Dominggoes Isakh <drjisakh@gmail.com>
2025-11-26 17:48:52 +0100

    universal8890-common: Migrate to AIDL touch
    Change-Id: Ia26f07a5bc5e33e980149c66dfef9e5c992714f8

--- device-common.mk
+++ device-common.mk
@@ -384,7 +384,7 @@ PRODUCT_PACKAGES += \
 
 # Touch features
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch-service.samsung
 
 # USB
 PRODUCT_PACKAGES += \
