3851f7f2117531aab0f749dd07b20e099bd148dc
Dominggoes Isakh <drjisakh@gmail.com>
2025-12-12 20:58:52 +0100

    universal8890-common: Update location of Lineage's device_compatibility_matrix
    Change-Id: I05c8d24498a4ba3e1b4a30eb2656222581ead3b6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,7 +86,7 @@ AB_OTA_UPDATER := false
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml 
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
